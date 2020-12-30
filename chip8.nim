5542802ff0845284d16e524c928051971ffdeedd
Jordan Burns <burns.s.jordan@gmail.com>
2020-12-30 12:34:12 -0700

    Add more opcodes, chip8 fontest, basic key input, and test opengl graphics.

--- chip8.nim
+++ chip8.nim
@@ -1,5 +1,7 @@
 import strformat
 import streams
+import random
+import rdstdin
 
 type
         Chip8 = object
@@ -18,8 +20,24 @@ type
 
 var chip8_fontset: array[0..79, uint8]
 
-for i in countup(0, 79):
-        chip8_fontset[i] = 1
+chip8_fontset = [uint8(0xF0), uint8(0x90), uint8(0x90), uint8(0x90), uint8(0xF0),
+  uint8(0x20), uint8(0x60), uint8(0x20), uint8(0x20), uint8(0x70),
+  uint8(0xF0), uint8(0x10), uint8(0xF0), uint8(0x80), uint8(0xF0),
+  uint8(0xF0), uint8(0x10), uint8(0xF0), uint8(0x10), uint8(0xF0),
+  uint8(0x90), uint8(0x90), uint8(0xF0), uint8(0x10), uint8(0x10),
+  uint8(0xF0), uint8(0x80), uint8(0xF0), uint8(0x10), uint8(0xF0),
+  uint8(0xF0), uint8(0x80), uint8(0xF0), uint8(0x90), uint8(0xF0),
+  uint8(0xF0), uint8(0x10), uint8(0x20), uint8(0x40), uint8(0x40),
+  uint8(0xF0), uint8(0x90), uint8(0xF0), uint8(0x90), uint8(0xF0),
+  uint8(0xF0), uint8(0x90), uint8(0xF0), uint8(0x10), uint8(0xF0),
+  uint8(0xF0), uint8(0x90), uint8(0xF0), uint8(0x90), uint8(0x90),
+  uint8(0xE0), uint8(0x90), uint8(0xE0), uint8(0x90), uint8(0xE0),
+  uint8(0xF0), uint8(0x80), uint8(0x80), uint8(0x80), uint8(0xF0),
+  uint8(0xE0), uint8(0x90), uint8(0x90), uint8(0x90), uint8(0xE0),
+  uint8(0xF0), uint8(0x80), uint8(0xF0), uint8(0x80), uint8(0xF0),
+  uint8(0xF0), uint8(0x80), uint8(0xF0), uint8(0x80), uint8(0x80)]
+
+randomize()
 
 var myChip8 = Chip8(drawFlag: false)
 
@@ -53,12 +71,39 @@ proc emulateCycle(chip8: var Chip8): int =
 
         # Decode and Execute Opcode
         case chip8.opcode and 0xF000
+        of 0x0000:
+                case chip8.opcode and 0x00FF:
+                of 0x00EE:
+                        chip8.sp = chip8.sp - 1
+                        chip8.pc = chip8.stack[chip8.sp]
+                        chip8.pc += 2
+                else:
+                        echo fmt("Unknown opcode {chip8.opcode:#x}")
+        of 0x1000:
+                chip8.pc = chip8.opcode and 0x0FFF
         of 0x2000:
                 chip8.stack[chip8.sp] = chip8.pc
                 chip8.sp = chip8.sp + 1
                 chip8.pc = chip8.opcode and 0x0FFF
+        of 0x3000:
+                if(chip8.V[(chip8.opcode and 0x0F00) shr 8] == uint8(chip8.opcode shr 16)):
+                        chip8.pc += 2
+                chip8.pc += 2
+        of 0x4000:
+                if(chip8.V[(chip8.opcode and 0x0F00) shr 8] != uint8(chip8.opcode shr 16)):
+                        chip8.pc += 2
+                chip8.pc += 2
+        of 0x6000:
+                chip8.V[(chip8.opcode and 0x0F00) shr 8] = uint8(chip8.opcode shr 16)
+                chip8.pc += 2
+        of 0x7000:
+                chip8.V[(chip8.opcode and 0x0F00) shr 8] += uint8(chip8.opcode shr 16)
+                chip8.pc += 2
         of 0x8000:
                 case chip8.opcode and 0x000F
+                of 0x0000:
+                        chip8.V[(chip8.opcode and 0x0F00) shr 8] = chip8.V[(chip8.opcode and 0x00F0) shr 4]
+                        chip8.pc += 2
                 of 0x0004:
                         if(chip8.V[(chip8.opcode and 0x00F0) shr 4] > (uint8(0xFF) - chip8.V[(chip8.opcode and 0x0F00) shr 8])):
                                 chip8.V[0xF] = 1
@@ -66,13 +111,19 @@ proc emulateCycle(chip8: var Chip8): int =
                                 chip8.V[0xF] = 0
                         chip8.V[(chip8.opcode and 0x0F00) shr 8] += chip8.V[(chip8.opcode and 0x00F0) shr 4]
                         chip8.pc += 2
-                        echo fmt("Opcode needs testing: {chip8.opcode:#x}")
                 else:
                         echo fmt("Unknown opcode {chip8.opcode:#x}")
+        of 0x9000:
+                if(chip8.V[(chip8.opcode and 0x0F00) shr 8] != chip8.V[(chip8.opcode and 0x00F0) shr 4]):
+                        chip8.pc += 2
+                chip8.pc += 2
         of 0xA000:
                 chip8.I = chip8.opcode and 0x0FFF
                 chip8.pc += 2
-                echo fmt("Opcode needs testing: {chip8.opcode:#x}")
+        of 0xC000:
+                let r = rand(255)
+                chip8.V[(chip8.opcode and 0x0F00) shr 8] = uint8(uint8(r) and uint8(chip8.opcode shr 16))
+                chip8.pc += 2
         of 0xD000:
                 var x: uint16 = chip8.V[(chip8.opcode and 0x0F00) shr 8]
                 var y: uint16 = chip8.V[(chip8.opcode and 0x00F0) shr 4]
@@ -90,15 +141,35 @@ proc emulateCycle(chip8: var Chip8): int =
 
                 chip8.drawFlag = true
                 chip8.pc += 2
-                echo fmt("Opcode needs testing: {chip8.opcode:#x}")
+        of 0xE000:
+                case chip8.opcode and 0x00FF:
+                of 0x009E:
+                        if(chip8.key[chip8.V[(chip8.opcode and 0x0F00) shr 8]] != uint8(0)):
+                                chip8.pc += 2
+                        chip8.pc += 2
+                        echo "need key wrong"
+                of 0x00A1:
+                        if(chip8.key[chip8.V[(chip8.opcode and 0x0F00) shr 8]] == uint8(0)):
+                                chip8.pc += 2
+                        chip8.pc += 2
+                else:
+                        echo fmt("Unknown opcode {chip8.opcode:#x}")
         of 0xF000:
                 case chip8.opcode and 0x00FF
+                of 0x0007:
+                        chip8.V[(chip8.opcode and 0x0F00) shr 8] = chip8.delay_timer
+                        chip8.pc += 2
+                of 0x0015:
+                        chip8.delay_timer = chip8.V[(chip8.opcode and 0x0F00) shr 8]
+                        chip8.pc += 2
+                of 0x001E:
+                        chip8.I += chip8.V[(chip8.opcode and 0x0F00) shr 8]
+                        chip8.pc += 2
                 of 0x0033:
                         chip8.memory[chip8.I] = uint8(int(chip8.V[(chip8.opcode and 0x0F00) shr 8]) / 100)
                         chip8.memory[chip8.I + 1] = uint8(int(int(chip8.V[(chip8.opcode and 0x0F00) shr 8]) / 10) mod 10)
                         chip8.memory[chip8.I + 2] = uint8((int(chip8.V[(chip8.opcode and 0x0F00) shr 8]) mod 100) mod 10)
                         chip8.pc += 2
-                        echo fmt("Opcode needs testing: {chip8.opcode:#x}")
                 else:
                         echo fmt("Unknown opcode {chip8.opcode:#x}")
         else:
@@ -112,9 +183,11 @@ proc emulateCycle(chip8: var Chip8): int =
         if chip8.sound_timer > uint8(0):
                 chip8.sound_timer = chip8.sound_timer - 1
 
-        echo "Cycle"
         return 0
 
 proc setKeys(chip8: var Chip8): int =
-        echo "Keys"
+        var userResponse: string
+        doAssert readLineFromStdin("Input: ", line = userResponse)
+        if(userResponse[0] == 'a'):
+                chip8.key[9] = 1
         return 0
--- main.nim
+++ main.nim
@@ -1,13 +1,45 @@
 include chip8
+import nimgl/[glfw, opengl]
 
+
+proc keyProc(window: GLFWWindow, key: int32, scancode: int32,
+             action: int32, mods: int32): void {.cdecl.} =
+  if key == GLFWKey.ESCAPE and action == GLFWPress:
+    window.setWindowShouldClose(true)
 
 proc setupGraphics(): int =
+        assert glfwInit()
+
+        glfwWindowHint(GLFWContextVersionMajor, 3)
+        glfwWindowHint(GLFWContextVersionMinor, 3)
+        glfwWindowHint(GLFWOpenglForwardCompat, GLFW_TRUE) # Used for Mac
+        glfwWindowHint(GLFWOpenglProfile, GLFW_OPENGL_CORE_PROFILE)
+        glfwWindowHint(GLFWResizable, GLFW_FALSE)
+
+        let w: GLFWWindow = glfwCreateWindow(800, 600, "NimGL")
+        if w == nil:
+                quit(-1)
+
+        discard w.setKeyCallback(keyProc)
+        w.makeContextCurrent()
+
+        assert glInit()
+
+        while not w.windowShouldClose:
+                glfwPollEvents()
+                glClearColor(0.68f, 1f, 0.34f, 1f)
+                glClear(GL_COLOR_BUFFER_BIT)
+                w.swapBuffers()
+
+        w.destroyWindow()
+        glfwTerminate()
         return 0
 
 proc setupInput(): int = 
         return 0
 
 proc drawGraphics(): int = 
+        echo "\e[2J"
         var line: string = ""   
         for i in countup(0, 31):
                 line = ""
@@ -26,8 +58,10 @@ proc main(): int =
         discard myChip8.initialize()
         discard myChip8.loadGame("pong")
 
-        for i in countup(1, 10):
-                discard myChip8.emulateCycle()
+        var gameRunning: bool = true
+        while gameRunning:
+                if(myChip8.emulateCycle() != 0):
+                        gameRunning = false
 
                 if myChip8.drawFlag:
                         discard drawGraphics()
